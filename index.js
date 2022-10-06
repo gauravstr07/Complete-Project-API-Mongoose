bedbea3d9d67919a378f9ab5d3de9a3e275394df
Gaurav Sutar <gauravstr05@gmail.com>
2022-10-06 17:03:08 +0530

    GET Data From Database

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-const express = require('express');
-require('./src/config');
-const Product = require('./src/product')
+const express = require("express");
+require("./src/config");
+const Product = require("./src/product");
 
 const app = express();
 app.use(express.json());
 const port = 5000;
 
 app.get("/", (req, res) => {
-    res.send("Hello from Express Server");
-})
+  res.send("Hello from Express Server");
+});
 
-app.post("/create", async(req, res) => {
-    let data = new Product(req.body);
-    let result = await data.save();
-    console.log(req.body);
-    res.send(result);
-   
-})
+app.post("/create", async (req, res) => {
+  let data = new Product(req.body);
+  let result = await data.save();
+  console.log(req.body);
+  res.send(result);
+});
+
+app.get("/list", async (req, res) => {
+  let data = await Product.find();
+  res.send(data);
+});
 
 app.listen(port, () => {
-    console.log(`Server running on port: ${port}`);
-})
+  console.log(`Server running on port: ${port}`);
+});
